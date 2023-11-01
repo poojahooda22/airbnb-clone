ecbec825d3fa188afa52bd6a21666b946042efd9
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-11-01 16:00:08 +0530

    create avatar

---
+++ app/components/Avatar.tsx
@@ -0,0 +1,20 @@
+'use client';
+
+import Image from "next/image";
+
+
+const Avatar = () => {
+    return (
+        <div>
+            <Image
+                className="rounded-full"
+                height="30"
+                width="30"
+                alt="Avatar"
+                src="/images/placeholder.jpg"
+            />
+        </div>
+    );
+}
+
+export default Avatar;
