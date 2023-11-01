c548f1d1b624970826e5891a8e813e0b63a2fefe
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-11-01 16:40:10 +0530

    updated

--- app/components/navbar/MenuItem.tsx
+++ app/components/navbar/MenuItem.tsx
@@ -10,7 +10,8 @@ const MenuItem: React.FC<MenuItemProps> = ({
     label,
 }) => {
     return ( 
-        <div className="px-4 py-3 hover:bg-neutral-100 transition font-semibold">
+        <div 
+            className="px-4 py-3 hover:bg-neutral-100 transition font-semibold">
             {label}
         </div>
      );
