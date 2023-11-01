359d05eba558ee356b9d86ef2db4ec841afd16ae
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-11-01 16:24:05 +0530

    updated usermenu

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -35,8 +35,9 @@ const UserMenu = () => {
             </div>
 
             {isOpen && (
-                <div className="absolute">
-                    <div>
+                <div className="absolute rounded-xl shadow-md w-[40vw] md:w-3/4
+                bg-white overflow-hidden right-0 top-12 text-sm">
+                    <div className="flex- flex-col cursor-pointer">
                         <>
                             <MenuItem />
                         </>
