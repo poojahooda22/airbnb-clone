f0d0db6575097be042743658e094ad9c1b382552
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-11-01 15:39:51 +0530

    update search

--- app/components/navbar/Search.tsx
+++ app/components/navbar/Search.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+import { BiSearch } from 'react-icons/bi';
+
 const Search = () => {
     return (
         <div className="border-[1px] w-full md:w-auto py-2 
@@ -16,6 +18,9 @@ const Search = () => {
                 items-center gap-3">
                     <div className="hidden sm:block">
                         Add Guests
+                    </div>
+                    <div className='p-2 bg-rose-500 rounded-full text-white'>
+                        <BiSearch size={18} />
                     </div>
                 </div>
             </div>
