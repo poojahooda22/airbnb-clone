0cbc11064d8dbe92f397428338e110cd03cf86c8
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-11-02 10:59:17 +0530

    update logo

--- app/components/navbar/Logo.tsx
+++ app/components/navbar/Logo.tsx
@@ -6,16 +6,16 @@ import { useRouter } from 'next/navigation';
 
 const Logo = () => {
     return (
-        <div>
-            <Image 
-                src="/images/logo.png"
-                alt="airbnb logo"
-                width={100}
-                height={100}
-                objectFit="contain"
-                className="cursor-pointer"
-            />
-        </div>
+       
+        <Image 
+            src="/images/logo.png"
+            alt="airbnb logo"
+            width={100}
+            height={100}
+            objectFit="contain"
+            className="cursor-pointer"
+        />
+    
     )
 }
 
