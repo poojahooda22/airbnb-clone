e0bd1e7bbd491926b3b98dcfb232f883eb7da0d9
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-11-02 15:45:54 +0530

    update register modal

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -11,6 +11,7 @@ import Modal from './Modal';
 import Heading from '../Heading';
 import Input from '../inputs/Input';
 import { toast } from 'react-hot-toast';
+import Button from '../Button';
 
 
 
@@ -72,13 +73,44 @@ const RegisterModal = () => {
             />
             <Input
                 id='password'
-                label='Passwword'
+                label='Password'
                 type='password'
                 disabled={isLoading}
                 required
                 register={register}
                 errors={errors}
             />
+        </div>
+    );
+
+    const footerContent = (
+        <div className='flex flex-col gap-4 mt-3'>
+            <hr />
+            <Button
+                outline
+                label="Continue with Google"
+                icon={FcGoogle}
+                onClick={() => {}}
+            />
+            <Button
+                outline
+                label="Continue with Github"
+                icon={AiFillGithub}
+                onClick={() => {}}
+            />
+            <div className="text-neutral-500 text-center mt-4 font-light">
+                <div className='justify-center flex flex-row items-center gap-2'>
+                    <div>
+                        Already have an account?
+                    </div>
+                    <div 
+                        onClick={registerModal.onClose}
+                        className='text-neutral-800 cursor-pointer hover:underline'
+                    >
+                        Log in
+                    </div>
+                </div>
+            </div>
         </div>
     )
 
@@ -91,6 +123,7 @@ const RegisterModal = () => {
             actionLabel='Continue'
             onSubmit={handleSubmit(onSubmit)}
             body={bodyContent}
+            footer={footerContent}
         />
      );
 }
