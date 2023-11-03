1aaa7b117e56243376ffc0ec3d86632579d4a612
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-11-03 20:21:14 +0530

    update navbar

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,25 +1,48 @@
-'use client';
+// import { SafeUser } from "@/app/types";
 
+// import Categories from "./Categories";
 import Container from "../Container";
-import Logo from "../navbar/Logo";
+import Logo from "./Logo";
 import Search from "./Search";
 import UserMenu from "./UserMenu";
+import { User } from "@prisma/client";
 
-
-const Navbar = () => {
-    return (
-        <div className="fixed w-full bg-white z-10 shadow-sm">
-           <div className="py-4 border-b-[1px]">
-                <Container>
-                    <div className="flex flex-row items-center justify-between gap-3 md:gap-0">
-                        <Logo />
-                        <Search />
-                        <UserMenu/>
-                    </div>
-                </Container>
-           </div>
-        </div>
-    );
+interface NavbarProps {
+  currentUser?: User | null;
 }
+
+const Navbar: React.FC<NavbarProps> = ({
+  currentUser,
+}) => {
+  return ( 
+    <div className="fixed w-full bg-white z-10 shadow-sm">
+      <div
+        className="
+          py-4 
+          border-b-[1px]
+        "
+      >
+      <Container>
+        <div 
+          className="
+            flex 
+            flex-row 
+            items-center 
+            justify-between
+            gap-3
+            md:gap-0
+          "
+        >
+          <Logo />
+          <Search />
+          <UserMenu currentUser={currentUser} />
+        </div>
+      </Container>
+    </div>
+    {/* <Categories /> */}
+  </div>
+  );
+}
+
 
 export default Navbar;
