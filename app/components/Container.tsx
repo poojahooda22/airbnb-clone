d8c54eaf6273b3dc6f84ef7ebc2aad58eee1e364
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-11-03 20:20:19 +0530

    update container

--- app/components/Container.tsx
+++ app/components/Container.tsx
@@ -1,19 +1,24 @@
-"use client"
+'use client';
 
 interface ContainerProps {
-    children: React.ReactNode;
-}
+  children: React.ReactNode
+};
 
-const Container: React.FC<ContainerProps> =({
-    children
-}) => {
-    return (
-        <div 
-            className="max-w-[25020px] mx-auto xl:px-20 
-            md:px-10 sm:px-2 px-4">
-            {children}
-        </div>
-    )
+const Container: React.FC<ContainerProps> = ({ children }) => {
+  return ( 
+    <div
+      className="
+        max-w-[2520px]
+        mx-auto
+        xl:px-20 
+        md:px-10
+        sm:px-2
+        px-4
+      "
+    >
+      {children}
+    </div>
+   );
 }
-
+ 
 export default Container;
