d1e801770795252e1dd50513a9286df7b54228cc
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-11-03 20:21:07 +0530

    update menu

--- app/components/navbar/MenuItem.tsx
+++ app/components/navbar/MenuItem.tsx
@@ -1,23 +1,28 @@
-'use client'
+'use client';
 
 interface MenuItemProps {
-    onClick: () => void;
-    label: string;
+  onClick: () => void;
+  label: string;
 }
 
 const MenuItem: React.FC<MenuItemProps> = ({
-    onClick,
-    label,
+  onClick,
+  label
 }) => {
-    return ( 
-        <div 
-            onClick={onClick}
-            className="px-4 py-3 hover:bg-neutral-100 transition 
-            font-semibold"
-        >
-            {label}
-        </div>
-     );
+  return ( 
+    <div 
+      onClick={onClick} 
+      className="
+        px-4 
+        py-3 
+        hover:bg-neutral-100 
+        transition
+        font-semibold
+      "
+    >
+      {label}
+    </div>
+   );
 }
  
 export default MenuItem;
